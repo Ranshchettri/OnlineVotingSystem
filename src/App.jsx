ce9a6f3ce459ecdfabdeb16c4eaa86dceabe0aa5
Ransh Chettri <ranshsunar@gmail.com>
2026-01-31 15:35:34 +0545

    Fix App.jsx: Admin router only

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
+import { BrowserRouter } from "react-router-dom";
+import AdminApp from "./admin/AdminApp";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <AdminApp />
     </BrowserRouter>
   );
 }
